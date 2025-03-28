39f96aa003d2fbb23768b1e91e0e4923dab1bb20
M4HDIOUI <omahdiou@student.1337.ma>
2025-03-28 22:14:34 +0000

    done

--- ex00/Program.java
+++ ex00/Program.java
@@ -1,6 +1,6 @@
 public class Program {
     public static void main(String[] args) {
-        int num = 123456789;
+        int num = 123456;
         int sum = 0;
         while(num > 0) {
             sum += num % 10;
